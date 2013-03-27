2b3171381a4c6d021f5dfed07f2cb5019f748829
Peter Halliday <pghalliday@gmail.com>
2013-03-27 22:36:22 +0100

    fixed day averaging

--- BitcoinTrading.js
+++ BitcoinTrading.js
@@ -20,8 +20,8 @@ if (Meteor.isClient) {
       day.setUTCMonth(date.getUTCMonth());
       day.setUTCDate(date.getUTCDate());
 
-      if (day === dayVolume[0]) {
-        dayQuantity[1] += trade.amount;
+      if (day.getTime() === dayQuantity[0].getTime()) {
+        dayQuantity[1] += parseFloat(trade.amount);
         value += (trade.amount * trade.price);
         dayPrice[1] = value / dayQuantity[1];
       } else {
@@ -40,34 +40,34 @@ if (Meteor.isClient) {
     d3.json('trades.json', function(trades) {
       var data  = transformTrades(trades);
       nv.addGraph(function() {
-        var chart = nv.models.linePlusBarChart()
-              .margin({top: 30, right: 60, bottom: 50, left: 70})
-              .x(function(d,i) { return i })
-              .y(function(d) { return d[1] })
-              .color(d3.scale.category10().range());
+          var chart = nv.models.linePlusBarChart()
+                .margin({top: 30, right: 60, bottom: 50, left: 70})
+                .x(function(d,i) { return i })
+                .y(function(d) { return d[1] })
+                .color(d3.scale.category10().range());
 
-        chart.xAxis
-          .showMaxMin(false)
-          .tickFormat(function(d) {
-            var dx = data[0].values[d] && data[0].values[d][0] || 0;
-            return d3.time.format('%x')(new Date(dx))
-          });
+          chart.xAxis
+            .showMaxMin(false)
+            .tickFormat(function(d) {
+              var dx = data[0].values[d] && data[0].values[d][0] || 0;
+              return d3.time.format('%x')(new Date(dx))
+            });
 
-        chart.y1Axis
-            .tickFormat(d3.format(',f'));
+          chart.y1Axis
+              .tickFormat(d3.format(',f'));
 
-        chart.y2Axis
-            .tickFormat(function(d) { return '$' + d3.format(',f')(d) });
+          chart.y2Axis
+              .tickFormat(function(d) { return '€' + d3.format(',f')(d) });
 
-        chart.bars.forceY([0]);
+          chart.bars.forceY([0]);
 
-        d3.select('#chart svg')
-          .datum(data)
-          .transition().duration(500).call(chart);
+          d3.select('#chart svg')
+              .datum(data)
+            .transition().duration(500).call(chart);
 
-        nv.utils.windowResize(chart.update);
+          nv.utils.windowResize(chart.update);
 
-        return chart;
+          return chart;
       });
     });
   }
