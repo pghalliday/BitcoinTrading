9f0d020a366d7bc1d1c4e72f92333e75be8b7734
Peter Halliday <pghalliday@gmail.com>
2013-03-27 17:23:20 +0100

    trying out the nvd3 reusable charts

--- BitcoinTrading.js
+++ BitcoinTrading.js
@@ -1,90 +1,55 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-function timeSeriesChart() {
-  var time, value, label;
-
-  var x = d3.time.scale()
-      .range([0, width]);
-
-  var y = d3.scale.linear()
-      .range([height, 0]);
-
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
-
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
-
-  var line = d3.svg.line()
-      .x(function(d) { return x(time(d)); })
-      .y(function(d) { return y(value(d)); });
-
-  function chart(selection) {
-    selection.each(function(d, i) {
-      x.domain(d3.extent(d, function(d) { return time(d); }));
-      y.domain(d3.extent(d, function(d) { return value(d); }));
-
-      var svg = selection.append('svg')
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      svg.append("g")
-          .attr("class", "y axis")
-          .call(yAxis)
-        .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", ".71em")
-          .style("text-anchor", "end")
-          .text(label);
-
-      svg.append("path")
-          .datum(d)
-          .attr("class", "line")
-          .attr("d", line);
-    });
-  };
-
-  chart.time  = function(callback) {
-    time = callback;
-    return chart;
-  }
-
-  chart.value  = function(callback) {
-    value = callback;
-    return chart;
-  }
-
-  chart.label = function(text) {
-    label = text;
-    return chart;
-  }
-
-  return chart;
-}
-
 if (Meteor.isClient) {
+  function transformTrades(trades) {
+    var data = [{
+      key: "Quantity",
+      bar: true,
+      values: []
+    }, {
+      key: "Price",
+      values: []
+    }];
+
+    trades.forEach(function(trade) {
+      data[0].values.push([new Date(trade.date * 1000), parseFloat(trade.amount)])
+      data[1].values.push([new Date(trade.date * 1000), parseFloat(trade.price)])
+    });
+
+    return data;
+  }
+  
   Template.chart.rendered = function () {
     d3.json('trades.json', function(trades) {
-      var chart = timeSeriesChart()
-        .time(function(d) { return new Date(d.date * 1000); })
-        .value(function(d) { return parseFloat(d.price); })
-        .label('Euros');
+      var data  = transformTrades(trades);
+      nv.addGraph(function() {
+        var chart = nv.models.linePlusBarChart()
+              .margin({top: 30, right: 60, bottom: 50, left: 70})
+              .x(function(d,i) { return i })
+              .y(function(d) { return d[1] })
+              .color(d3.scale.category10().range());
 
-      d3.select(".chart-wrapper")
-        .datum(trades)
-        .call(chart);
+        chart.xAxis
+          .showMaxMin(false)
+          .tickFormat(function(d) {
+            var dx = data[0].values[d] && data[0].values[d][0] || 0;
+            return d3.time.format('%x')(new Date(dx))
+          });
+
+        chart.y1Axis
+            .tickFormat(d3.format(',f'));
+
+        chart.y2Axis
+            .tickFormat(function(d) { return '$' + d3.format(',f')(d) });
+
+        chart.bars.forceY([0]);
+
+        d3.select('#chart svg')
+          .datum(data)
+          .transition().duration(500).call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
     });
   }
 }
